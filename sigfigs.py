f95b7bac92f02a4b8ce23242e7319af4d558c340
Garrett Thompson <garett.leland.thompson@gmail.com>
2017-07-29 09:37:07 -0700

    removed print statements, simplified function calling

--- sigfigs.py
+++ sigfigs.py
@@ -33,7 +33,6 @@ def sfCount(number):
         return noDecimal(numb_list)
     elif 'e' in numb_list:
         # exponential notation
-        print("here!")
         return numb_list.index('e')-1
         #-1 for decimal place
     else:
@@ -71,44 +70,43 @@ def noDecimal(numb_list):
     #calculate number of digits between non zeros, inclusive
     return last_non_zero_location - first_non_zero_location + 1
 
+def sfCalc(number1,number2,operation):
+    if operation = "*" or operation = "/":
+        return sfMultDIv(number1,number2,operation)
+    elif operation = "+" or operation = "-"):
+        return sfAddSub(number1,number2,operation)
+    else:
+        print("Uh oh, check your operator parameter and try again")
+        break
 
-
-def sfMult(number1,number2,operation):
+def sfMultDiv(number1,number2,operation):
         #return value with smallest sig figs
         sf1,sf2 = count_sigfigs(number1),count_sigfigs(number2)
         sigfig = min(sf1,sf2)
         evaluation = eval(number1+operation+number2)
         exp_string = "%.*e" %(sigfig-1,evaluation) 
-        print("eval_str= ",exp_string)
         if "." in exp_string : 
             exp_string = exp_string.replace(".","")
         front_str,back_str = exp_string.split('e')
         exponent = int(back_str)
-        print(front_str,back_str)
         if exponent == 0:
-            print("no exponent")
             # no exponent, can truncate
             return front_str
         elif exponent > 0:
-            print("pos. exponent")
             if exponent > len(front_str):
-                print("exp>len(front_str") 
                 # +1 for decimal place
                 answer = front_str+(exponent-len(front_str)+1)*"0" 
             elif exponent < len(front_str):
-                print("exp<len(front_str") 
                 # have to add decimal back in
                 answer = front_str[:1+exponent]+"."+front_str[1+exponent:]
             elif exponent == len(front_str):
-                print("exp=len(front_str") 
                 answer = front_str + "." 
         elif exponent < 0:
-            print("neg. exp")
             answer =  "0." + (abs(exponent)-1)*"0" + front_str
 
         return answer
 
-def sfAdd(number1,number2,operation):
+def sfAddSub(number1,number2,operation):
         number1,number2 = str(number1).lower(),str(number2).lower()
         numb1,numb2 = number1,number2
         #return value with fewest decimal places 
