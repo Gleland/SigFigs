451bc2e40be16da6100030801334d0ccb56507fe
Garrett Thompson <garett.leland.thompson@gmail.com>
2017-08-06 12:27:44 -0700

    added descriptions to functions and reorganized. Begain writing new sfRound() function

--- sigfigs.py
+++ sigfigs.py
@@ -18,6 +18,12 @@
 
 
 def sfCount(number):
+    """
+    Counts number of sig figs for a given number
+
+    INPUTS: number
+    OUTPUTS: number of sig figs of input
+    """
     number = str(number).lower()
     # deleting neg. sign, not important for sig figs
     if ('-' in str(number)):     
@@ -39,7 +45,14 @@ def sfCount(number):
         print("Something broke")
 
 def hasDecimal(numb_list,decimal_location):
+    """
+    Helper function for determining number of sig figs
+    the case when the input has a decimal
 
+    INPUTS: numb_list (python list of number)
+            decimal_location (integer of location in numb_list
+    OUTPUTS: number of sig figs in input 
+    """
     for index,digit in enumerate(numb_list):
         if  digit !='0': 
             first_non_zero_location = index
@@ -55,7 +68,13 @@ def hasDecimal(numb_list,decimal_location):
         return len(numb_list) - first_non_zero_location 
 
 def noDecimal(numb_list):
-
+    """
+    Helper function for determining number of sig figs
+        the case when the input has a decimal
+    INPUTS: numb_list (python list of number)
+    OUTPUTS: number of sig figs in input 
+    
+    """
     for index,digit in enumerate(numb_list):
         if  digit !='0': 
             first_non_zero_location = index
@@ -71,59 +90,116 @@ def noDecimal(numb_list):
     return last_non_zero_location - first_non_zero_location + 1
 
 def sfCalc(number1,number2,operation):
-    if operation = "*" or operation = "/":
+    """
+    Performs mathematical operation and retains sig figs
+    INPUTS: number1,number2 (input numbers)
+            operation (str of operator to be performed)
+    OUTPUTS: calculated answer to correct sig figs
+    """
+    if operation == "*" or operation == "/":
         return sfMultDIv(number1,number2,operation)
-    elif operation = "+" or operation = "-"):
+    elif operation == "+" or operation == "-":
         return sfAddSub(number1,number2,operation)
     else:
         print("Uh oh, check your operator parameter and try again")
-        break
 
 def sfMultDiv(number1,number2,operation):
-        #return value with smallest sig figs
-        sf1,sf2 = count_sigfigs(number1),count_sigfigs(number2)
-        sigfig = min(sf1,sf2)
-        evaluation = eval(number1+operation+number2)
-        exp_string = "%.*e" %(sigfig-1,evaluation) 
-        if "." in exp_string : 
-            exp_string = exp_string.replace(".","")
-        front_str,back_str = exp_string.split('e')
-        exponent = int(back_str)
-        if exponent == 0:
-            # no exponent, can truncate
-            return front_str
-        elif exponent > 0:
-            if exponent > len(front_str):
-                # +1 for decimal place
-                answer = front_str+(exponent-len(front_str)+1)*"0" 
-            elif exponent < len(front_str):
-                # have to add decimal back in
-                answer = front_str[:1+exponent]+"."+front_str[1+exponent:]
-            elif exponent == len(front_str):
-                answer = front_str + "." 
-        elif exponent < 0:
-            answer =  "0." + (abs(exponent)-1)*"0" + front_str
+    """
+    Determines multiplication and division operations
+    while conserving sig figs
+    
+    Because the operation is multiplying/dividing, 
+    sig fig rules state that the answer should have the sig figs
+    of the smallest input. EX: 6.154 * 3.14 = 19.3
 
-        return answer
+    INPUTS: number1,number2 (numbers in operation)
+            operation ("*" or "/")
+    OUTPUTS: answer to calculation with correct sig figs
+    """
+    #return value with smallest sig figs
+    sf1,sf2 = sfCount(number1),sfCount(number2)
+    sigfig = min(sf1,sf2)
+    evaluation = eval(number1+operation+number2)
+    exp_string = "%.*e" %(sigfig-1,evaluation) 
+    if "." in exp_string : 
+        exp_string = exp_string.replace(".","")
+    front_str,back_str = exp_string.split('e')
+    exponent = int(back_str)
+    if exponent == 0:
+        # no exponent, can truncate
+        return front_str
+    elif exponent > 0:
+        if exponent > len(front_str):
+            # +1 for decimal place
+            answer = front_str+(exponent-len(front_str)+1)*"0" 
+            if sfCount(answer) != sigfig:
+                answer = "%.*e" %(sigfig-1,evaluation) 
+        elif exponent < len(front_str):
+            # have to add decimal back in
+            answer = front_str[:1+exponent]+"."+front_str[1+exponent:]
+        elif exponent == len(front_str):
+            answer = front_str + "." 
+    elif exponent < 0:
+        answer =  "0." + (abs(exponent)-1)*"0" + front_str
+
+    return answer
 
 def sfAddSub(number1,number2,operation):
-        number1,number2 = str(number1).lower(),str(number2).lower()
-        numb1,numb2 = number1,number2
-        #return value with fewest decimal places 
-        if "e" in number1: 
-            numb1, _ = number1.split("e")
-        if "e" in number2:
-            numb2, _ = number2.split("e")
-        if ("." in number1) and ("." in number2):
-            temp1,temp2 = numb1.split("."),numb2.split(".")
-            dec1,dec2 = len(temp1[-1]),len(temp2[-1])
-            min_dec = min(dec1,dec2)
-        else:
-            min_dec=0
-        # if this ^^ isn't met, then at least one number doesn't have a decimal
-        evaluation = eval(number1+operation+number2)
-        return "%.*f" %(min_dec,evaluation) 
-        #return value with fewest decimal places
+    """
+    Determines addition and subtraction operations
+    while conserving sig figs
+
+    Because the oepration is adding/subtracting, sig fig rules
+    state that the answer should retain the fewest deciaml places
+    between inputs. Ex: 1.00 + 1.0 = 2.0
+    
+    INPUTS: number1,number2 (numbers in operation)
+            operation ("*" or "/")
+    OUTPUTS: answer to calculation with correct sig figs
+    """
+    number1,number2 = str(number1).lower(),str(number2).lower()
+    numb1,numb2 = number1,number2
+    #return value with fewest decimal places 
+    if "e" in number1: 
+        numb1, _ = number1.split("e")
+    if "e" in number2:
+        numb2, _ = number2.split("e")
+    if ("." in number1) and ("." in number2):
+        temp1,temp2 = numb1.split("."),numb2.split(".")
+        dec1,dec2 = len(temp1[-1]),len(temp2[-1])
+        min_dec = min(dec1,dec2)
+    else:
+        min_dec=0
+    # if this ^^ isn't met, then at least one number doesn't have a decimal
+    evaluation = eval(number1+operation+number2)
+    return "%.*f" %(min_dec,evaluation) 
+    #return value with fewest decimal places
         
+
+def sfRound(number,sigfigs):
+    """
+    Round input number to given number of sig figs
+
+    INPUTS: number
+            sigfigs
+    OUTPUTS: number with correct sig figs
+    """
+    number=str(number).lower()
+    if 'e' in number:
+        front_str,back_str = exp_string.split('e')
+        exponent = int(back_str)
+    # shift 6.0E6 -> 6.0e6
+    multiple = "1." + (int(sigfigs)-1)*"0"
+    # minus one to account for decimal place
+    return sfMultDiv(number,multiple,"*")
+
+
+
+
+
+
+
+
+
 
 
