7e3b702f722a30ece4191bdd59f430aa588a1508
Garrett Thompson <garrett.leland.thompson@gmail.com>
2020-01-01 19:26:04 -0800

    removing intial testing functions

--- tests/test_sigfigs.py
+++ tests/test_sigfigs.py
@@ -1,12 +1,6 @@
 import pytest
 from sigfigs import sigfigs
 
-def func(x):
-    return x + 1
-
-def test_answer():
-    assert func(3) == 4
-
 def test_sigfigs_add():
     sf = sigfigs.SigFigs()
     assert(sf.add("0.01","1") == '1')
