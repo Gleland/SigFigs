aadaf7100f7e07674a0dad0d5fea51ce03a3ef3a
Garrett Thompson <garrett.leland.thompson@gmail.com>
2020-01-01 20:26:30 -0800

    PEP8 fixes

--- tests/test_sigfigs.py
+++ tests/test_sigfigs.py
@@ -1,34 +1,37 @@
-import pytest
 from sigfigs import sigfigs
 
 
 def test_sigfigs_add():
     sf = sigfigs.SigFigs()
-    assert(sf.add("0.01","1") == '1')
+    assert(sf.add("0.01", "1") == '1')
+
 
 def test_sigfigs_subtract():
     sf = sigfigs.SigFigs()
-    assert(sf.subtract("0.01","1") == '-1')
+    assert(sf.subtract("0.01", "1") == '-1')
+
 
 def test_sigfigs_multiply():
     sf = sigfigs.SigFigs()
     test_cases = [
         {"inputs": ["10.0", "1"], "answer": "1e+01"},
         {"inputs": ["0.00203", "0.8888"], "answer": "1.80e-03"},
-        {"inputs": ["6.154","3.14"], "answer": "1.93e+01"}
+        {"inputs": ["6.154", "3.14"], "answer": "1.93e+01"}
     ]
     for case in test_cases:
         assert(sf.multiply(*case["inputs"]) == case["answer"])
+
 
 def test_sigfigs_divide():
     sf = sigfigs.SigFigs()
     test_cases = [
         {"inputs": ["20.0", "4"], "answer": "5e+00"},
-        {"inputs": ["01000.","-0.006700"], "answer": "-1.493e+05"},
-        {"inputs": ["90090.","5420"], "answer": "1.66e+01"},
+        {"inputs": ["01000.", "-0.006700"], "answer": "-1.493e+05"},
+        {"inputs": ["90090.", "5420"], "answer": "1.66e+01"},
     ]
     for case in test_cases:
         assert(sf.divide(*case["inputs"]) == case["answer"])
+
 
 def test_sigfigs_count():
     sf = sigfigs.SigFigs()
@@ -50,6 +53,7 @@ def test_sigfigs_count():
     }
     for case, count in test_cases.items():
         assert(sf.count(case) == count)
+
 
 def test_sigfigs_round():
     sf = sigfigs.SigFigs()
